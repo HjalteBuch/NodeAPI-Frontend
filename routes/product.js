b844c017a6ef98ca8408b5271425660254089c9a
Hjalte Hesbjerg <hjalte@Hjaltes-MacBook-Pro.local>
2023-12-03 20:58:54 +0900

    Added integretion with my NodeApi with tiny-json-http module. Also implemented bootstrap stylesheets

--- routes/product.js
+++ routes/product.js
@@ -1,24 +1,33 @@
 const router = require('express').Router();
 
-router.get('/', (req, res, next) => {
-    res.render('product.mustache',
-        {
-            "data": [
-                {
-                    "productID": 354,
-                    "name": "Long-Sleeve Logo Jersey, L"
+const tiny = require('tiny-json-http');
+
+const url = 'http://localhost:3000/api/product';
+
+router.get('/', async (req, res, next) => {
+    try {
+        let response = await tiny.get({
+            "url": url
+        });
+        let data = response.body.data;
+        console.log(data);
+        res.render('product',
+            {
+                "isListVisible": true,
+                "data": data,
+                "costAsCurrency": function () {
+                    return new Number(this.standardCost).toLocalString("en-US",
+                        { "style": "currency", "currencty": "USD" });
                 },
-                {
-                    "productID": 356,
-                    "name": "HL Road Frame - Red, 62"
-                },
-                {
-                    "productID": 358,
-                    "name": "HL Road Frame - Red, 48"
+                "priceAsCurrencty": function () {
+                    return new Number(this.listPrice).toLocalString("en-us",
+                        { "style": "currency", "currencty": "USD" });
                 }
-            ]
-        }
-    )
+            }
+        );
+    } catch (err) {
+        next(err);
+    }
 });
 
 module.exports = router;
